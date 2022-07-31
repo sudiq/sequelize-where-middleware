7145786f9c26724422eda9472b9583acb881b93a
Familusi Babatunde <burntblark@gmail.com>
2022-07-31 10:18:59 +0100

    feat: blacklist fields

--- sqlize.js
+++ sqlize.js
@@ -1,10 +1,20 @@
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
-function goDeeper(deepObject, key = '', res = {}) {
+function goDeeper(deepObject, key = '', options, res = {}) {
   if (key.indexOf('$') > -1) {
     operator = key.substring(1);
     key = Op[operator];
+  }
+
+  const blacklist = [];
+
+  if (options) {
+    blacklist = options.blacklist || [];
+  }
+
+  if (blacklist.includes(key)) {
+    return res;
   }
   
   if (Array.isArray(deepObject)) {
@@ -13,20 +23,20 @@ function goDeeper(deepObject, key = '', res = {}) {
   
   let temp = {};
   for (let op in deepObject) {
-    let newres = goDeeper(deepObject[op], op, res);
+    let newres = goDeeper(deepObject[op], op, options, res);
     temp = { ...temp, ...newres };
   }
   return { ...{ [key]: temp }, ...res };
 }
 
-exports.retrieveWhere = function (whereStr) {
+exports.retrieveWhere = function (whereStr, options) {
   if (!whereStr) {
     return;
   }
   
   try {
     const whereObj = JSON.parse(whereStr);
-    const result = goDeeper(whereObj, "where");
+    const result = goDeeper(whereObj, "where", options);
     return result;
   } catch (err) {
     if (err instanceof SyntaxError) {
